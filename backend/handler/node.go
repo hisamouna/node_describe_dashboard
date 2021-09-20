d7eff94a845fa541d0cb4e6328eac1d3a40c84f3
hikaru-sasamoto <hikaru.sasa9s@gmail.com>
2021-09-20 17:54:02 +0900

    fix error message

--- backend/handler/node.go
+++ backend/handler/node.go
@@ -21,14 +21,14 @@ func (handler *NodeHandler) DescribeNodes(ctx context.Context, in *emptypb.Empty
 	}
 	nodes, err := kc.GetNodes()
 	if err != nil {
-		return nil, fmt.Errorf("GetNodes(kueb.GetNodes): %v", err)
+		return nil, fmt.Errorf("DescribeNode(kueb.GetNodes): %v", err)
 	}
 
 	var response = &node.NodesResponse{}
 	for _, nd := range nodes {
 		dn, err := kc.DescribeNode(nd)
 		if err != nil {
-			return nil, fmt.Errorf("Describe(kube.DescribeNode): %v", err)
+			return nil, fmt.Errorf("DescribeNode(kube.DescribeNode): %v", err)
 		}
 		var np = []*node.Pod{}
 		for _, p := range dn.Pods {
